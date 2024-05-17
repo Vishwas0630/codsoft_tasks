f1e467590b850e233dff04f98af3801ccb41ba14
Vishwas0630 <168364558+Vishwas0630@users.noreply.github.com>
2024-05-17 13:04:20 +0530

    studentgradecalculator.java
    codesoft task2 to create the student calculator
    Input: Take marks obtained (out of 100) in each subject.
    Calculate Total Marks: Sum up the marks obtained in all subjects.
    Calculate Average Percentage: Divide the total marks by the total number of subjects to get the
    average percentage.
    Grade Calculation: Assign grades based on the average percentage achieved.
    Display Results: Show the total marks, average percentage, and the corresponding grade to the user

---
+++ studentgradecalculator.java
@@ -0,0 +1,42 @@
+import java.util.Scanner;
+
+public class GradeCalculator {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Enter the number of subjects:");
+        int numOfSubjects = scanner.nextInt();
+        
+        int totalMarks = 0;
+        
+        for (int i = 1; i <= numOfSubjects; i++) {
+            System.out.println("Enter marks obtained in subject " + i + ":");
+            int marks = scanner.nextInt();
+            totalMarks += marks;
+        }
+        
+        double averagePercentage = (double) totalMarks / numOfSubjects;
+        
+        System.out.println("\nTotal Marks: " + totalMarks);
+        System.out.println("Average Percentage: " + averagePercentage + "%");
+        
+        char grade;
+        if (averagePercentage >= 90) {
+            grade = 'A';
+        } else if (averagePercentage >= 80) {
+            grade = 'B';
+        } else if (averagePercentage >= 70) {
+            grade = 'C';
+        } else if (averagePercentage >= 60) {
+            grade = 'D';
+        } else if (averagePercentage >= 50) {
+            grade = 'E';
+        } else {
+            grade = 'F';
+        }
+        
+        System.out.println("Grade: " + grade);
+        
+        scanner.close();
+    }
+}
